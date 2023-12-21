e2904d05a831c5c145e90d5bdaedacd4a516b641
Kyle Jacobson <jacokyle01@gmail.com>
2023-12-21 15:55:20 -0600

    add initializer that marks all nodes as unseen

---
+++ src/util.ts
@@ -0,0 +1,24 @@
+import { transform } from "chessops/pgn";
+import { PgnNodeData } from "chessops/pgn";
+
+interface Context {
+	clone(): Context;
+}
+
+const context: Context = {
+	clone() {
+		const clonedCtx: Context = { ...this };
+		return clonedCtx;
+	},
+};
+
+const markUnseen = (ctx: Context, data, childIndex) => {
+	return {
+		...data,
+		comments: data.comments ? data.comments + "/*unseen,0*/" : "/*unseen,0*/",
+	};
+};
+
+export const initializeTraining = (head: Node) => {
+    return transform(head, context, markUnseen);
+}
