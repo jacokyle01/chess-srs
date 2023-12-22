dedf3b7b1505608e669a9582aadc5be50c8104e4
Kyle Jacobson <jacokyle01@gmail.com>
2023-12-22 17:24:49 -0600

    add utility function to find&mutate node by pgn path

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
-import { transform } from "chessops/pgn";
-import { PgnNodeData, Node } from "chessops/pgn";
+import { Game, transform, walk } from "chessops/pgn";
+import { PgnNodeData, Node, ChildNode } from "chessops/pgn";
 
 
 
@@ -51,3 +51,31 @@ export const annotateWithPaths = (node: Node<PgnNodeData>) => {
         };
     })
 }
+
+//function to find and modify node by path 
+export const transformNode = (subrep: Game<PgnNodeData>, path: string, f: (target: Node<PgnNodeData>) => Node<PgnNodeData>) => {
+	const steps = path.split(" ");
+	console.log(steps);
+	console.log(subrep.moves.children.find(child => child.data.san == steps[0])); 
+
+	let current: ChildNode<PgnNodeData> | undefined;
+	current = subrep.moves.children.find(child => child.data.san == steps[0])
+	for (let i = 1; i < steps.length; i++) {
+		const step = steps[i];
+		current = current?.children.find(child => child.data.san == step);
+	}	
+	// current.data = f(current);
+	current.data = f(current);
+	console.log(current.data.san);
+	console.log(subrep.moves.children[0].children[0].children[0])
+}
+
+// const callback = (target: Node<PgnNodeData>) => {
+// 	console.log("test");
+// 	return {
+// 		...target,
+// 		training: {
+// 			"test": "test"
+// 		}
+// 	}
+// }
