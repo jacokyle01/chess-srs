b5b11b711936932654523881ce97db0a1ff6c12c
Kyle Jacobson <jacokyle01@gmail.com>
2023-12-22 23:36:21 -0600

    add api to set repertoire- initializes as side-effect

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,39 @@
+import { Game, PgnNodeData, parsePgn } from "chessops/pgn";
+import { State } from "./state";
+import { TrainingData, initializeTraining } from "./util";
+
 export interface Api {
-    sayHello(): void
+    sayHello(): void;
+    sayNumber(): void
+    initialize(k: number): void; //load kth subrepertoire into 'TrainingOrder'. all nodes will be unseen
+    load(k: number): void; //load kth subreperoire into `TrainingOrder` 
+    setTime(time: number): boolean //set time of state. boolean: whether or not this new time is different 
+    setRepertoire(pgn: string): boolean //load repertoire into memory, annotates as unseen. boolean: whether not this was a valid PGN
+    getRepertoire(): Game<PgnNodeData>[];
 }
 
 
-export function start(): Api {
+export function start(state: State): Api {
     return {
-        sayHello: () => console.log("hello")
+        sayHello: () => console.log("hello"),
+        sayNumber: () => console.log(state.count),
+        setTime: (time: number) => {
+            if (state.time == time) {
+                state.time = time;
+                return true;
+            }
+            return false;
+        },
+        setRepertoire: (pgn: string) => {
+            const games = parsePgn(pgn);
+            const annotated: Game<PgnNodeData>[] = [];
+            for (const game of games) {
+                game.moves = initializeTraining(game.moves);
+            }
+            state.repertoire = games;
+        },
+        getRepertoire: () => {
+            return state.repertoire;
+        }
     }
 }
