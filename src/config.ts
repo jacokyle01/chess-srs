86f2bf8b462a1261c8ae8d966994aad97b8092a9
Kyle Jacobson <jacokyle01@gmail.com>
2023-12-30 20:06:44 -0600

    allow for configuration of state

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { State } from "./state"
+
 export interface Config {
     learn?: { //when variation is unsee 
         by?: "depth" | "breadth", //how we find the next variation
@@ -11,3 +13,27 @@ export interface Config {
     promotion?: "most" | "next"
     demotion?: "most" | "next"
 }
+
+export function configure(state: State, config: Config): void {
+    deepMerge(state, config);
+}
+
+function deepMerge(base: any, extend: any): void {
+    for (const key in extend) {
+      if (Object.prototype.hasOwnProperty.call(extend, key)) {
+        if (
+          Object.prototype.hasOwnProperty.call(base, key) &&
+          isPlainObject(base[key]) &&
+          isPlainObject(extend[key])
+        )
+          deepMerge(base[key], extend[key]);
+        else base[key] = extend[key];
+      }
+    }
+  }
+
+  function isPlainObject(o: unknown): boolean {
+    if (typeof o !== 'object' || o === null) return false;
+    const proto = Object.getPrototypeOf(o);
+    return proto === Object.prototype || proto === null;
+  }
--- src/trainer.ts
+++ src/trainer.ts
@@ -1,8 +1,9 @@
 import { Api, start } from "./api";
-import { Config } from "./config";
+import { Config, configure } from "./config";
 import { State, defaults } from "./state";
 
 export function ChessSrs(config?: Config): Api {
 	const state = defaults();
+	configure(state, config || {})
 	return start(state);
 }
