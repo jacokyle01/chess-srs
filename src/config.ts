801f3af0550ce1ff1f32258cbea4ebf33db12488
Kyle Jacobson <jacokyle01@gmail.com>
2024-04-05 16:14:12 -0500

    formatting

--- src/config.ts
+++ src/config.ts
@@ -1,41 +1,41 @@
-import { State } from "./state.js";
+import { State } from './state.js';
 
 export interface Config {
-	learn?: {
-		//when variation is unsee
-		by?: "depth" | "breadth"; //how we find the next variation
-		max?: number; //dont look at variations after this many moves
-	};
-	recall?: {
-		//after a variation has already been seen
-		by?: "depth" | "breadth";
-		max?: number;
-	};
-	buckets?: number[]; //the "spaces" for spaced repetition. see "leitner system"
-	promotion?: "most" | "next";
-	demotion?: "most" | "next";
+  learn?: {
+    //when variation is unsee
+    by?: 'depth' | 'breadth'; //how we find the next variation
+    max?: number; //dont look at variations after this many moves
+  };
+  recall?: {
+    //after a variation has already been seen
+    by?: 'depth' | 'breadth';
+    max?: number;
+  };
+  buckets?: number[]; //the "spaces" for spaced repetition. see "leitner system"
+  promotion?: 'most' | 'next';
+  demotion?: 'most' | 'next';
 }
 
 export function configure(state: State, config: Config): void {
-	deepMerge(state, config);
+  deepMerge(state, config);
 }
 
 function deepMerge(base: any, extend: any): void {
-	for (const key in extend) {
-		if (Object.prototype.hasOwnProperty.call(extend, key)) {
-			if (
-				Object.prototype.hasOwnProperty.call(base, key) &&
-				isPlainObject(base[key]) &&
-				isPlainObject(extend[key])
-			)
-				deepMerge(base[key], extend[key]);
-			else base[key] = extend[key];
-		}
-	}
+  for (const key in extend) {
+    if (Object.prototype.hasOwnProperty.call(extend, key)) {
+      if (
+        Object.prototype.hasOwnProperty.call(base, key) &&
+        isPlainObject(base[key]) &&
+        isPlainObject(extend[key])
+      )
+        deepMerge(base[key], extend[key]);
+      else base[key] = extend[key];
+    }
+  }
 }
 
 function isPlainObject(o: unknown): boolean {
-	if (typeof o !== "object" || o === null) return false;
-	const proto = Object.getPrototypeOf(o);
-	return proto === Object.prototype || proto === null;
+  if (typeof o !== 'object' || o === null) return false;
+  const proto = Object.getPrototypeOf(o);
+  return proto === Object.prototype || proto === null;
 }
