f1396395f9c075a6cad194962410d741c76ca1dd
Kyle Jacobson <jacokyle01@gmail.com>
2024-05-15 18:41:42 -0500

    reduce next() to basic BFS, that is, don't include context of previous runs

--- src/api.ts
+++ src/api.ts
@@ -10,7 +10,8 @@ export interface Api {
   update(time?: number): boolean; //set time of state, or set time to now. boolean: whether or not this new time is different
   setMethod(method: Method): void; //set training method. learn or recall
   state(): State; //get the state of this instance
-  next(): boolean | null; //advance trainer to next path. returns whether or not there was another trainable path, undefined if no subrepertoire.
+  // next(): boolean | null; //advance trainer to next path. returns whether or not there was another trainable path, undefined if no subrepertoire.
+  next(): ChildNode<TrainingData>[] | null; //if possible, get next trainable path, else return empty
   path(): ChildNode<TrainingData>[] | null; //get the current path
   succeed(): void; //handle training success based on context
   fail(): void; //handle training fail based on context
@@ -52,89 +53,136 @@ export function start(state: State): Api {
       state.path = null;
     },
     next: () => {
-      let index = state.index;
-      if (index == -1) return false;
-      let queue = state.queue;
-      let first = true; //flag for whether or not this dequeued element is the first
-      let flag = false; //record whether or not we've done a complete exploration of the opening tree
-
-      //initialize queue if empty
-      if (queue.length == 0) {
-        flag = true;
-        for (const child of state.repertoire[index].moves.children) {
-          const queueEntry: QueueEntry = {
-            path: [child],
-            layer: 0,
-          };
-          queue.push(queueEntry);
-        }
+      if (state.index == -1) return null; // no subrepertoire selected
+      //initialization
+      let queue: QueueEntry[] = [];
+      let subrep = state.repertoire[state.index];
+      //initialize queue
+      for (const child of subrep.moves.children) {
+        queue.push({
+          path: [child],
+          layer: 0,
+        });
       }
+      while (queue.length != 0) {
+        //initialize dequeued path
+        const entry = queue.shift()!;
+        const pos = entry.path.at(-1)!;
 
-      let parent: ChildNode<TrainingData>;
-      let id = -1;
-
-      while (queue.length > 0) {
-        const maybeEntry: QueueEntry | undefined = queue.shift();
-        if (!maybeEntry) {
-          return null;
-        }
-        const entry: QueueEntry = maybeEntry;
-        const maybeParent: ChildNode<TrainingData> | undefined = entry.path.at(-1);
-        if (!maybeParent) {
-          return null;
-        }
-        parent = maybeParent;
-        if (first) {
-          id = parent.data.training.id;
-          first = false;
-        } else {
-          //handle loop around
-          if (parent.data.training.id == id) {
-            return false;
+        //test if match
+        if (!pos.data.training.disabled) {
+          switch (state.method) {
+            case 'recall': //recall if due
+              if (pos.data.training.dueAt <= state.time) {
+                state.path = entry.path;
+                // return true;
+                return entry.path;
+              }
+              break;
+            case 'learn': //learn if unseen
+              if (!pos.data.training.seen) {
+                state.path = entry.path;
+                // return true;
+                return entry.path;
+              }
+              break;
           }
         }
 
-        for (const child of parent.children) {
+        //enqueue child nodes
+        for (const child of pos.children) {
           const queueEntry: QueueEntry = {
             path: [...entry.path, child],
             layer: ++entry.layer,
           };
           queue.push(queueEntry);
         }
-
-        if (!parent.data.training.disabled) {
-          switch (state.method) {
-            case 'recall': //recall if due
-              if (parent.data.training.dueAt <= state.time) {
-                state.path = entry.path;
-                return true;
-              }
-              break;
-            case 'learn': //learn if unseen
-              if (!parent.data.training.seen) {
-                state.path = entry.path;
-                return true;
-              }
-              break;
-          }
-        }
-
-        if (queue.length == 0) {
-          if (flag) {
-            return false;
-          } else {
-            flag = true;
-            for (const child of state.repertoire[index].moves.children) {
-              const queueEntry: QueueEntry = {
-                path: [child],
-                layer: 0,
-              };
-              queue.push(queueEntry);
-            }
-          }
-        }
       }
       return null;
+
+      // let index = state.index;
+      // if (index == -1) return false;
+      // let queue = state.queue;
+      // let first = true; //flag for whether or not this dequeued element is the first
+      // let flag = false; //record whether or not we've done a complete exploration of the opening tree
+
+      // //initialize queue if empty
+      // if (queue.length == 0) {
+      //   flag = true;
+      //   for (const child of state.repertoire[index].moves.children) {
+      //     const queueEntry: QueueEntry = {
+      //       path: [child],
+      //       layer: 0,
+      //     };
+      //     queue.push(queueEntry);
+      //   }
+      // }
+
+      // let parent: ChildNode<TrainingData>;
+      // let id = -1;
+
+      // while (queue.length > 0) {
+      // const maybeEntry: QueueEntry | undefined = queue.shift()!;
+      //   if (!maybeEntry) {
+      //     return null;
+      //   }
+      //   const entry: QueueEntry = maybeEntry;
+      //   const maybeParent: ChildNode<TrainingData> | undefined = entry.path.at(-1);
+      //   if (!maybeParent) {
+      //     return null;
+      //   }
+      //   parent = maybeParent;
+      //   if (first) {
+      //     id = parent.data.training.id;
+      //     first = false;
+      //   } else {
+      //     //handle loop around
+      //     if (parent.data.training.id == id) {
+      //       return false;
+      //     }
+      //   }
+
+      //   for (const child of parent.children) {
+      //     const queueEntry: QueueEntry = {
+      //       path: [...entry.path, child],
+      //       layer: ++entry.layer,
+      //     };
+      //     queue.push(queueEntry);
+      //   }
+
+      //   if (!parent.data.training.disabled) {
+      //     switch (state.method) {
+      //       case 'recall': //recall if due
+      //         if (parent.data.training.dueAt <= state.time) {
+      //           state.path = entry.path;
+      //           return true;
+      //         }
+      //         break;
+      //       case 'learn': //learn if unseen
+      //         if (!parent.data.training.seen) {
+      //           state.path = entry.path;
+      //           return true;
+      //         }
+      //         break;
+      //     }
+      //   }
+
+      //   if (queue.length == 0) {
+      //     if (flag) {
+      //       return false;
+      //     } else {
+      //       flag = true;
+      //       for (const child of state.repertoire[index].moves.children) {
+      //         const queueEntry: QueueEntry = {
+      //           path: [child],
+      //           layer: 0,
+      //         };
+      //         queue.push(queueEntry);
+      //       }
+      //     }
+      //   }
+      // }
+      // return null;
     },
     load: (k: number) => {
       if (k >= state.repertoire.length) {
